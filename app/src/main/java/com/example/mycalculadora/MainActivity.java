85c90919d08a19992d4cfa62a2f26f88729ce19b
Andres Camargo <ingcamargolastra@gmail.com>
2019-07-06 14:23:03 -0500

    Seteado el combobox

--- app/src/main/java/com/example/mycalculadora/MainActivity.java
+++ app/src/main/java/com/example/mycalculadora/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.mycalculadora;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,13 +13,23 @@ public class MainActivity extends AppCompatActivity {
     TextView resultado;
     EditText num1,num2;
     double n1, n2, suma;
+    String opciones[];
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Captura de los objetos utilizados
         num1 = findViewById(R.id.txtNumeroUno);
         num2 = findViewById(R.id.txtNumeroDos);
         resultado = findViewById(R.id.txtResultado);
+        spinner = findViewById(R.id.cmbOperacion);
+        //Traemos las opciones en un Array de Strings
+        opciones = getResources().getStringArray(R.array.operaciones);
+        //Creamos el adapter indicando donsde se va a colocar como se va a visualizar y que se va a mostrar
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item, opciones);
+        //pasamos el adapter al combo
+        spinner.setAdapter(adapter);
     }
 
     public void calcular(View v){
@@ -26,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         suma = n1 + n2;
         resultado.setText(""+suma);
     }
+
 
 
 }
