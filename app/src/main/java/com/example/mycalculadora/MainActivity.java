7bd0a5308fc090fbed8dfba4d015f17e85e7fc15
Andres Camargo <ingcamargolastra@gmail.com>
2019-07-06 16:13:14 -0500

    Calculadora Básica terminada

--- app/src/main/java/com/example/mycalculadora/MainActivity.java
+++ app/src/main/java/com/example/mycalculadora/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.mycalculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -33,12 +34,56 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void calcular(View v){
-        n1 = Double.parseDouble(num1.getText().toString());
-        n2 = Double.parseDouble(num2.getText().toString());
-        suma = n1 + n2;
-        resultado.setText(""+suma);
+        double res = 0;
+        int opc;
+        if (validar()){
+            n1 = Double.parseDouble(num1.getText().toString());
+            n2 = Double.parseDouble(num2.getText().toString());
+            opc = spinner.getSelectedItemPosition();
+            switch (opc){
+                case 0:
+                    res = n1+n2;
+                    break;
+                case 1:
+                    res = n1-n2;
+                    break;
+                case 2:
+                    res = n1*n2;
+                    break;
+                case 3:
+                    res = n1/n2;
+                    break;
+            }
+            //Log por consola
+            Log.d("DivisionLog", "Probamos el mensaje"+res);
+            resultado.setText(""+res);
+        }
     }
 
+    public void limpiar(View v){
+        num1.setText("");
+        num2.setText("");
+        resultado.setText("");
+        spinner.setSelection(0);
+        num1.requestFocus();
+    }
 
+    public boolean validar(){
+        int opc = spinner.getSelectedItemPosition();
+        if (num1.getText().toString().isEmpty()){
+            num1.setError(getResources().getString(R.string.error_1));
+            num1.requestFocus();
+            return false;
+        }else if(num2.getText().toString().isEmpty()){
+            num2.setError(getResources().getString(R.string.error_2));
+            num2.requestFocus();
+            return false;
+        }else if(opc == 3 && Double.parseDouble(num2.getText().toString())==0){
+            num2.setError(getResources().getString(R.string.error_div_0));
+            num2.selectAll();
+            return false;
+        }
+        return true;
+    }
 
 }
